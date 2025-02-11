7e53ffcf1e822881bc86b59fdcaa63879239a336
Firdous Nath <firdous.nath@alpha-vector.com>
2025-02-11 19:35:34 +0530

    first commit

---
+++ user-data/data.js
@@ -0,0 +1,112 @@
+export const bio = [
+  "Hello, I'm Firdous Nath!",
+  "Full-Stack Engineer with <strong>8+ years</strong> of experience in the design, development & deployment of high quality mobile & web applications. Proven expertise in system architecture including both low-level (LLD) & high-level (HLD) design, data structures & algorithms with a strong foundation in software engineering principles.",
+];
+
+export const skills = [
+  "<strong>Programming Languages:</strong> Dart, JAVA, Kotlin, JavaScript & SQL",
+  "<strong>Frameworks & Libraries:</strong> Flutter, React-native, NodeJS, Retrofit, Get_it, i18n, Freezed, Mocktail & JUnit5",
+  "<strong>Architectures & State Management:</strong> MVC,MVP, MVVM, Redux, BloC, Getx & Provider",
+];
+
+export const experience = [
+  {
+    title: "Tide",
+    duration: "January 2023 - Present",
+    subtitle: "Full-Stack Engineer III",
+    details: [
+      "Developed and maintained a fintech app with 2 Million+ lines of code across 300+ packages.",
+      "Architected and led the development of Tide Instant Checkout for the Acquiring team, a key initiative contributing to an ARR currently growing at £1 million.",
+      "Built a robust permission guard system for over 300 features using Dart build runner via custom annotation, ensuring secure and controlled access based on market availability (UK, Germany or India).",
+      "Organized and maintained over 50+ Confluence pages covering code documentation, product demos & future development roadmaps ensuring a clear & accessible knowledge repository for the team.",
+    ],
+    tags: ["Flutter"," Mason CLI", "Spring boot", "Docker", "AWS"],
+    icon: "qrcode",
+  },
+  {
+    title: "The Good Glamm Group",
+    duration: "June 2022 - December 2023",
+    subtitle: "Lead Engineer",
+    details: [
+      "Engineered India's largest influencer network app empowering over 2 million creators.",
+      "Configured flavours to distinctively manage Dev, Stage & Prod environments.",
+      "Setup CI/CD with Fastlane which reduces manual effort & time-to-market by 90%.",
+      "Implemented BDD tests, achieving ~70% code coverage & 99% crash reduction (Firebase Crashlytics) boosting stability & developer confidence for fearless refactoring.",
+    ],
+    tags: [
+      "Flutter", "PostgreSQL","GraphQL", "NodeJS", "AWS", "Fastlane", "Firebase",
+    ],
+    icon: "hashtag",
+  },
+  {
+    title: "Ninety One, AlphaVector Pvt. Ltd.",
+    duration: "June 2020 - April 2022",
+    subtitle: "Senior Software Engineer",
+    details: [
+    "Designed an algorithm to detect fake cycling rides with ~95% accuracy.",
+    "Architected a ride-saving strategy with a 100% success rate which enhanced user satisfaction.",
+    "Designed \"Athletes of the Week\" which increases the retention rate of athletes 80% on a weekly basis.",
+    "Transitioned Hub91 and Team91 Android apps from Java to Kotlin in just 3 months.",
+    ],
+    tags: ["JavaScript", "React-native", "AWS Amplify", "Bootstrap", "Nodejs"],
+    icon: "bicycle",
+  },
+  {
+    title: "CourseKart.com",
+    duration: "May 2017- May 2020",
+    subtitle: "Software Development Engineer",
+    details: [
+    "Engineered and delivered high-impact educational mobile apps used by over 200,000 students for renowned institutions like Resonance, JK Shah,The Gate Academy & Endurance.",
+    "Developed a desktop app to boost admin efficiency by 50% for managing videos, notes & tests.",
+    "Integrated a video processing pipeline that converts videos to the HLS format in three size variations, resulting in a 30% reduction in bandwidth usage & significantly faster loading times for end users.",
+    ],
+    tags: ["Android", "Realm", "Java", "Kotlin", "ElectronJS"],
+    icon: "graduation-cap",
+  },
+];
+
+
+export const education = [
+  {
+    title: "B.Tech. in Computer Science and Engineering",
+    duration: "2013 - 2017",
+    subtitle: "Rajasthan Technical University",
+    details: ["77.4% Honors"],
+    tags: [
+      "Data Structures & Algorithms",
+      "Operating Systems",
+      "Database Management System",
+      "Computer Networks",
+    ],
+    icon: "graduation-cap",
+  },
+  {
+    title: "Class 12th in Science and Mathematics",
+    duration: "2011-2013",
+    subtitle: "Central Board of Secondary Education",
+    details: ["81%"],
+    tags: ["Physics", "Chemistry", "Mathematics"],
+    icon: "book",
+  }
+];
+
+export const footer = [
+
+  {
+    label: "Links",
+    data: [
+        {
+          text: "Linkedin",
+          link: "https://www.linkedin.com/in/firdous-nath/",
+        },
+        {
+          text: "Github",
+          link: "https://github.com/firdousnath",
+        },
+    ],
+  },
+  {
+    label: "copyright-text",
+    data: ["Made with &hearts; by Firdous Nath"],
+  },
+];
